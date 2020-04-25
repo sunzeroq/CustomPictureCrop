a85997b2a50e9651ada3a7dbdf617b9b2b7f1ca6
sunzeroq <596594483@qq.com>
2020-04-26 00:43:12 +0800

    project porting

---
+++ src/components/customPic.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="custom-container">
+    <div class="custom-draw-header">
+      <div class="custom-draw-buttons">
+        <button @click="complete" :disabled="!status">完成</button>
+        <button @click="cancel" :disabled="!status">撤销</button>
+        <button @click="outPic" :disabled="status">出图</button>
+        <button @click="reset">重置</button>
+        <button @click="close">关闭</button>
+      </div>
+      <div class="custom-draw-info">
+        <span>操作说明:1.单击开始绘制边框2.在最后一点处双击完成3.点击出图,查看或保存图片</span>
+      </div>
+    </div>
+    <div class="custom-draw-content">
+      <div ref="canvasDiv" class="custom-canvas">
+        <canvas
+          ref="customCanvas"
+          id="custom"
+          style="width:100%;"
+          @click="savePoints"
+          @mousemove="saveMouse"
+          @dblclick.prevent="complete"
+        ></canvas>
+        <canvas ref="bufferCanvas" id="buffer" style="width:100%;opacity:0;position:absolute;"></canvas>
+        <canvas ref="clipCanvas" id="clip" style="width:100%;opacity:0;position:absolute;"></canvas>
+      </div>
+    </div>
+  </div>
+</template>
+<script src="../utils/canvas2image.js"></script>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    imgUrl: {
+      type: String,
+      default: ""
+    },
+    proportion: {
+      type: Number,
+      default: 1
+    }
+  },
+  components: {},
+  name: "customDraw",
+  data() {
+    return {
+      title: "自定义绘制",
+      points: [],
+      newImg: "",
+      height: 400,
+      width: 600,
+      mouse: { x: null, y: null },
+      id: null,
+      status: true
+    };
+  },
+  mounted() {
+    this.loadCanvas();
+  },
+  methods: {
+    //事件监听
+    savePoints(e) {
+      this.points.push({ x: e.layerX, y: e.layerY });
+    },
+    saveMouse(e) {
+      this.mouse = { x: e.layerX, y: e.layerY };
+    },
+    //设置canvas相关
+    setCanvas(id) {
+      let canvas = document.getElementById(id);
+      let context = canvas.getContext("2d");
+      let w = canvas.offsetWidth;
+      let h = w / this.proportion;
+      canvas.width = w;
+      canvas.height = h;
+      return [context, w, h, canvas];
+    },
+    clearWatchDog() {
+      clearInterval(this.id);
+      this.id = null;
+      this.points = null;
+    },
+    //入口
+    loadCanvas() {
+      this.id = setInterval(() => {
+        if (this.$refs.customCanvas) {
+          this.updateCanvas();
+        } else {
+          this.clearWatchDog();
+        }
+      }, 17);
+    },
+    updateCanvas() {
+      let that = this;
+      let [context, w, h, buffer] = this.setCanvas("buffer");
+      let img = new Image();
+      img.src = this.imgUrl;
+      //清除画布
+      context.clearRect(0, 0, w, h);
+      //加载元素
+      img.onload = function() {
+        context.drawImage(img, 0, 0, w, h);
+        that.drawLine(context);
+        //缓冲复制到主屏
+        that.copyCanvas(buffer);
+      };
+    },
+    drawLine(context) {
+      //绘制已确定位置的线
+      let p = this.points;
+      context.lineWidth = 2;
+      context.lineJoin = "round";
+      for (let i = 0; i < p.length; i++) {
+        context.lineTo(p[i].x, p[i].y);
+        context.moveTo(p[i].x + 4, p[i].y);
+        context.arc(p[i].x, p[i].y, 4, 0, 2 * Math.PI);
+        context.moveTo(p[i].x, p[i].y);
+      }
+      //绘制mousemove预览线
+      context.lineTo(this.mouse.x, this.mouse.y);
+      context.moveTo(this.mouse.x + 4, this.mouse.y);
+      context.arc(this.mouse.x, this.mouse.y, 4, 0, 2 * Math.PI);
+      context.moveTo(this.mouse.x, this.mouse.y);
+      context.stroke();
+    },
+    //双缓冲解决闪屏跳帧
+    copyCanvas(buffer) {
+      let [context] = this.setCanvas("custom");
+      context.drawImage(buffer, 0, 0);
+    },
+    //canvas根据路径clip()裁剪图片
+    complete() {
+      let [context, w, h] = this.setCanvas("clip");
+      //根据points绘制路径并裁剪
+      this.canvasClip(context);
+      this.updateImg(context, w, h);
+      this.status = false;
+      this.clearWatchDog();
+    },
+    canvasClip(context) {
+      context.beginPath();
+      for (let i = 0; i < this.points.length; i++) {
+        let element = this.points[i];
+        context.lineTo(element.x, element.y);
+      }
+      context.clip();
+      context.closePath();
+    },
+    updateImg(context, w, h) {
+      let img = new Image();
+      img.src = this.imgUrl;
+      img.onload = function() {
+        context.drawImage(img, 0, 0, w, h);
+      };
+    },
+    cancel() {
+      this.points.pop();
+      if (this.id === null) {
+        this.loadCanvas();
+      }
+    },
+    outPic() {
+      let canvas = document.getElementById("clip");
+      this.canvasToImg(canvas);
+      //更新pic
+      this.$emit("update:newUrl", this.newImg);
+      //出图
+      this.$emit("out-picture");
+      this.clearWatchDog();
+    },
+    canvasToImg(canvas) {
+      //只需要定义图片的宽度（高度计算得到）
+      let imageWidth = canvas.offsetWidth;
+      let imageHeight = imageWidth / this.proportion;
+      //转换图片
+      let genimg = Canvas2Image.convertToImage(
+        canvas,
+        imageWidth,
+        imageHeight,
+        "png"
+      );
+      this.newImg = genimg.src;
+    },
+    close() {
+      this.$emit("close-custom");
+      this.clearWatchDog();
+    },
+    reset() {
+      //两种情况下的reset
+      if (this.status) {
+        //画图状态
+        this.points = [];
+      } else {
+        this.points = [];
+        this.loadCanvas();
+        this.status = true;
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.custom-container {
+  background-color: #ffffff;
+  .custom-draw-header {
+    position: absolute;
+    z-index: 10004;
+    top: -32px;
+    right: 0;
+    .custom-draw-buttons {
+      display: flex;
+      display: -webkit-flex;
+      justify-content: flex-end;
+    }
+    .custom-draw-info {
+      margin: 10px;
+      padding: 0.75rem1.25rem;
+      border-radius: 4px;
+      background-color: white;
+      border-width: 0;
+      box-shadow: 02px6px0rgba (114, 124, 245, 0.5);
+    }
+  }
+  .custom-draw-content {
+    position: absolute;
+    width: 100%;
+    z-index: 10003;
+    background-color: #ffffff;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <custom-draw :show="a" :imgUrl="url" :proportion="c"></custom-draw>
+    <canvas id="canvasDemo"></canvas>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CustomDraw from "@/components/customPic.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: { CustomDraw },
+  data() {
+    return {
+      a: true,
+      url: "/logo.png",
+      c: 1
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      let canvas = document.getElementById("canvasDemo");
+      let context = canvas.getContext("2d");
+      let w = canvas.offsetWidth;
+      let h = w;
+      canvas.width = w;
+      canvas.height = h;
+      let img = new Image();
+      img.src = this.url;
+
+      console.log(img);
+
+      img.onload = function() {
+        context.drawImage(img, 0, 0, w, h);
+      };
+    }
   }
-}
+};
 </script>
